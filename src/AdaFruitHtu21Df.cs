7e69f06a167401ef4f942b9ec3e5dffc0554182b
oscar gigon <eggboy016@outlook.com>
2015-12-11 21:06:25 -0800

    implementing ReadTemp

--- src/AdaFruitHtu21Df.cs
+++ src/AdaFruitHtu21Df.cs
@@ -9,6 +9,7 @@ namespace Display
     {
         private const byte ReadReg = 0xe7;
         private const byte Reset = 0xfe;
+        private const byte ReadTemp = 0xE3;
         public AdaFruitHtu21Df()
         {
             I2cConfiguration = new I2CDevice.Configuration(I2C_ADDRESS, I2C_ClockRateKHz);
@@ -37,7 +38,34 @@ namespace Display
 
 
         }
-        public float readTemperature() { }
+        public float readTemperature() {
+  // OK lets ready!
+            var buffer = new byte[1];
+
+            buffer[0] = ReadTemp;
+            var transaction = I2CDevice.CreateWriteTransaction(buffer);
+            Bus.Execute(new[] { transaction }, 1000);
+            Thread.Sleep(50);
+            buffer[0] = 0;
+            var readTx = I2CDevice.CreateReadTransaction(buffer);
+            Bus.Execute(new[] { readTx }, 1000);
+  delay(50); // add delay between request and actual read!
+  
+  Wire.requestFrom(HTU21DF_I2CADDR, 3);
+  while (!Wire.available()) {}
+
+  uint16_t t = Wire.read();
+  t <<= 8;
+  t |= Wire.read();
+
+  uint8_t crc = Wire.read();
+
+  float temp = t;
+  temp *= 175.72;
+  temp /= 65536;
+  temp -= 46.85;
+
+  return temp; }
         public float readHumidity() { }
         public void reset()
         {
